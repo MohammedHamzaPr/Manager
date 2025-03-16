0ad9ddeb471693b6466fb69ccb8f82898518bbb4
Mohamed <60695273+MohammedHamzaPr@users.noreply.github.com>
2025-03-16 21:33:14 +0300

    Update Library.py

--- Library.py
+++ Library.py
@@ -1,14 +1,14 @@
 from tkinter import *
 from tkinter import ttk
-#import mysql.connector as mysql
+import sqlite3 as mysql
 from tkinter import messagebox
 from datetime import datetime
 from threading import Thread
 from os import getcwd
-'''try:
-    connect = mysql.connect(host='localhost',user='root',passwd='pass',database='hello')
+try:
+    connect = mysql.connect('data.db',check_same_thread=False)
     cursor = connect.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS data(id TEXT, name TEXT, age TEXT, job TEXT, email TEXT, gender TEXT, address TEXT, Fc TEXT, recruitment TEXT, pn TEXT) ')
 except:
     messagebox.showerror('Server','Please Turn On Server MySQL')
     exit()
-'''
